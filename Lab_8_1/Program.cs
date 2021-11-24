4b0875a2ab25a13d775f15143f8b5102b7e65c46
11111 <11111@DESKTOP-I2P1BG6>
2021-11-24 11:22:09 +0300

    Add project files.

---
+++ Lab_8_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\Lena\\(4)";
+            string root = "*.*";
+            string[] folders = Directory.GetDirectories(path, root, SearchOption.AllDirectories);
+                Console.WriteLine("folders");
+                foreach (string n in folders)
+                {
+                    Console.WriteLine(n);
+                }
+                string[] files = Directory.GetFiles(path, root, SearchOption.AllDirectories);
+                Console.WriteLine("files");
+                foreach (string m in files)
+                {
+                    Console.WriteLine(m);
+                }
+            Console.ReadKey();
+
+        }
+    }
+}
